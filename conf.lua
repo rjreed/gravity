d8a4a052deaf81028c3d10c7721a5b6ad1861f7f
rjreed <rockyjreed@gmail.com>
2014-11-19 18:45:30 -0600

    initial setup, simple test case for physics and some re-work on the animation lib

--- conf.lua
+++ conf.lua
@@ -1,7 +1,8 @@
+-- set config options
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = '0.9.1'                -- The LÖVE version this game was made for (string)
-    t.window.title = 'Untitled'        -- The window title (string)
+    t.window.title = 'gravity'         -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
     t.window.width = 1024              -- The window width (number)
     t.window.height = 768              -- The window height (number)
--- main.lua
+++ main.lua
@@ -1,11 +1,72 @@
-function love.load()
 
+require('libs/animate')
+
+function love.load()
+	 local push = table.insert
+	 
+	 love.physics.setMeter(32)
+	 world = love.physics.newWorld(0, 9.81*32, true)
+	 
+	 objects = {}
+
+	 player = {}
+	 player.body = love.physics.newBody(world, 100, 100, 'dynamic')
+	 player.body:setMass(10)
+   player.shape = love.physics.newRectangleShape(32, 64)
+   player.fixture = love.physics.newFixture(player.body, player.shape)
+	 player.img  = love.graphics.newImage('assets/player.png')
+   player.anim = newAnimation(player.img, 64, 64, 0.1, 1, 3)
+   player.flip = false
+
+	 push(objects, player)
+
+   static = {}
+   static.body = love.physics.newBody(world, 0 ,700, 'static')
+   static.shape = love.physics.newEdgeShape(0, 0, 1000, 0)
+   static.fixture = love.physics.newFixture(static.body, static.shape)
+end
+
+function move(obj)
+	 if love.keyboard.isDown('d') then
+      player.anim:setFrameRange(5, 7)      
+      player.flip = false
+			obj.body:applyForce(1000, 0)
+	 elseif love.keyboard.isDown('a') then
+      player.anim:setFrameRange(5, 7)      
+      player.flip = true
+			obj.body:applyForce(-1000, 0)
+	 end
+	 if love.keyboard.isDown('w') then
+			obj.body:applyForce(0, -1000)
+	 elseif love.keyboard.isDown('s') then
+			obj.body:applyForce(0, 1000)
+	 end
 end
 
 function love.update(dt)
+	 
+	 mouseX = love.mouse.getX()
+	 mouseY = love.mouse.getY()
+	 
+	 if love.mouse.isDown('l') then
+			for i, v in pairs(objects) do
+				 if v.fixture:testPoint(mouseX, mouseY) then
+						focused = v
+						break
+				 else focused = nil
+				 end
+			end
+	 end
+	 player.anim:setFrameRange(1,3)
+	 if love.keyboard.isDown('w', 'a', 's', 'd') then
+      move(player)
+   end
 
+	 world:update(dt)
+	 player.anim:update(dt)
 end
 
 function love.draw()
-
+	 player.anim:draw(player.body:getX() - 32, player.body:getY() - 32, 0, player.flip and  -1 or 1, 1, 32, 0) 
+   love.graphics.line(static.body:getWorldPoints(static.shape:getPoints()))
 end
